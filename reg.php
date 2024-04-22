996ed6b5eb1dad45ac497496ef2647a75172ee96
TAPPY\User <itapilot_ccs@uspf.edu.ph>
2024-04-23 01:17:29 +0800

    working reg data

--- reg.php
+++ reg.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection
 $servername = "localhost"; // Change this to your database server
-$username = "username"; // Change this to your database username
-$password = "password"; // Change this to your database password
-$dbname = "database_name"; // Change this to your database name
+$username = "root"; // Change this to your database username
+$password = ""; // Change this to your database password
+$dbname = "carrental"; // Change this to your database name
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -16,21 +16,30 @@ if ($conn->connect_error) {
 $firstName = $_POST['firstName'];
 $lastName = $_POST['lastName'];
 $email = $_POST['email'];
+$userName = $_POST['userName'];
 $password = $_POST['password'];
 
 // Prepare SQL statement to insert data into database
-$sql = "INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?)";
+$sql = "INSERT INTO registration (firstname, lastname, email, username, password) VALUES (?, ?, ?, ?, ?)";
 
 // Prepare and bind parameters
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("ssss", $firstName, $lastName, $email, $password);
+
+if (!$stmt) {
+    die("Error: " . $conn->error); // Check for errors in prepare statement
+}
+
+$stmt->bind_param("sssss", $firstName, $lastName, $email, $userName, $password);
 
 // Execute the query
 if ($stmt->execute()) {
     echo "Registration successful!";
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "Error: " . $stmt->error; // Use $stmt->error instead of $conn->error
 }
+
+// Close statement
+$stmt->close();
 
 // Close connection
 $conn->close();
